159c2012d08404a4b1eb67fffbb16521ee0004ec
Christian Linde <info@christian-linde.de>
2024-08-31 20:59:49 +0200

    start project

---
+++ src/DependencyInjection/UserGuideExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Norvutec\UserGuideBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class UserGuideExtension extends Extension {
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+
+
+}
--- src/NorvutecUserGuideBundle.php
+++ src/NorvutecUserGuideBundle.php
@@ -2,10 +2,20 @@
 
 namespace Norvutec\UserGuideBundle;
 
+use Norvutec\UserGuideBundle\DependencyInjection\UserGuideExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class NorvutecUserGuideBundle extends AbstractBundle {
 
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new UserGuideExtension();
+    }
 
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 
 }

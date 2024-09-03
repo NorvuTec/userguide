331574ecc7cc099de147164f6d9d9b9846dd13d1
Christian Linde <info@christian-linde.de>
2024-09-03 10:53:49 +0200

    fix form extension

--- src/Form/UserGuideFormExtension.php
+++ src/Form/UserGuideFormExtension.php
@@ -13,8 +13,7 @@ class UserGuideFormExtension extends AbstractTypeExtension {
     public const USER_GUIDE = 'user_guide';
     public const USER_GUIDE_STEP = 'user_guide_step';
 
-    public function __construct()
-    {
+    public function __construct() {
     }
 
     public static function getExtendedTypes(): iterable {
@@ -23,23 +22,22 @@ class UserGuideFormExtension extends AbstractTypeExtension {
 
     public function configureOptions(OptionsResolver $resolver) : void {
         $resolver->setDefined([self::USER_GUIDE, 'user_guide_step']);
-        $resolver->setAllowedTypes(self::USER_GUIDE, 'string|null');
-        $resolver->setAllowedTypes(self::USER_GUIDE_STEP, 'int|null');
+        $resolver->setAllowedTypes(self::USER_GUIDE, ['string', 'null']);
+        $resolver->setAllowedTypes(self::USER_GUIDE_STEP, ['int', 'null']);
         $resolver->setDefault(self::USER_GUIDE, null);
         $resolver->setDefault(self::USER_GUIDE_STEP, null);
     }
 
-    public function buildView(FormView $view, FormInterface $form, array $options): void
-    {
+    public function buildView(FormView $view, FormInterface $form, array $options): void {
         if(!in_array(self::USER_GUIDE, $options) || $options[self::USER_GUIDE] == null) {
             return;
         }
         if(!in_array('attr', $view->vars)) {
             $view->vars['attr'] = [];
         }
-        $view->vars['attr']['data-user-guide'] = $options[self::USER_GUIDE];
+        $view->vars['attr']['data-assigned-user-guide'] = $options[self::USER_GUIDE];
         if($options[self::USER_GUIDE_STEP] != null) {
-            $view->vars['attr']['data-user-guide-step'] = $options[self::USER_GUIDE_STEP];
+            $view->vars['attr']['data-assigned-user-guide-step'] = $options[self::USER_GUIDE_STEP];
         }
     }
 
